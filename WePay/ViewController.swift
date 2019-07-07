2c7bf28d79364dcd5aa0d39e93fd2f9e51bcf7fd
AquaQAQ <1632259373@qq.com>
2019-07-07 11:53:51 +0800

    Initial Commit

---
+++ WePay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WePay
+//
+//  Created by Wallance on 2019/7/7.
+//  Copyright © 2019 Wallance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
